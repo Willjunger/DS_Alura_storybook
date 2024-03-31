a44873e94c87ca0a73aee0daee29f3b9599b31ca
Willjunger <williamjunger@hotmail.com>
2024-03-31 20:22:04 -0300

    testando commits

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React, { FC, HTMLAttributes, ReactChild } from 'react';
 
 export interface Props extends HTMLAttributes<HTMLDivElement> {
-  /** custom content, defaults to 'the snozzberries taste like snozzberries' */
+  /** custom content, defaults to 'the snozzberries taste like snozzberries'. */
   children?: ReactChild;
 }
 
