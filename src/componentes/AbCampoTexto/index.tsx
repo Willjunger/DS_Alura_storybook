5f92e6cf87f76c470ce19cef94cd8d97f43bebf3
willjunger <williamjunger@hotmail.com>
2024-04-03 22:17:26 -0300

    retira espaçamento

--- src/componentes/AbCampoTexto/index.tsx
+++ src/componentes/AbCampoTexto/index.tsx
@@ -37,7 +37,6 @@ export const AbCampoTexto = ({ label, value, onChange, type = 'text' }: AbCampoT
         <div>
             <LabelEstilizada>
                 {label}
-
             </LabelEstilizada>
             <InputEstilizado type={type} value={value} onChange={(event: React.ChangeEvent<HTMLInputElement>) => onChange(event.target.value)} />
         </div>
